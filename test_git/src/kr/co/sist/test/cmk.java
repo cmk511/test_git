b2dabf80d7052bb2e8ddfad0a2aa7a48e5f71ad4
user <user@DESKTOP-COJ9858>
2023-08-22 12:40:53 +0900

    머지2

--- test_git/src/kr/co/sist/test/cmk.java
+++ test_git/src/kr/co/sist/test/cmk.java
@@ -5,7 +5,8 @@ public class cmk {
 	public static void main(String[] args) {
 		System.out.println("이것은 신기능");
 		//커밋은 무조건 메인에서 하는것
-
+		System.out.println("대포동미사일");
+		System.out.println("임태균의 미소!!!");
 	}
 
 }
